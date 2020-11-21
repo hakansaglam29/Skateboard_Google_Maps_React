96e3ff25aab50a86b149bca7159076339dc36851
Hakan <67585490+hakansaglam29@users.noreply.github.com>
2020-11-21 21:37:38 +0100

    google map and marker added

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,14 +1,53 @@
 import React, { useState, useEffect} from "react";
 import axios from "axios";
+import {GoogleMap, withScriptjs, withGoogleMap, Marker, InfoWindow} from "react-google-maps";
+import * as parksData from './assets/skateboard.json'
 
-function ()
+function Map() {
+  const [selectedPark, setSelectedPark] = useState(null)
+  return(
+    <GoogleMap 
+    defaultCenter={{lat:45.421532, lng:-75.697189}} 
+    defaultZoom={10}>
 
+      {parksData.features.map(park =>(
+      <Marker  
+      key={park.properties.PARK_ID}  
+      position={{
+        lat: park.geometry.coordinates[1], 
+        lng:park.geometry.coordinates[0]
+      }}
+      onClick={()=>{
+        setSelectedPark(park);
+      }}
+      />
+      ))}
+      
+      {selectedPark && (
+        <InfoWindow 
+          position={{
+            lat:selectedPark.geometry.coordinates[1], lng:selectedPark.geometry.coordinates[0]
+          }}
+          onCloseClick={()=>setSelectedPark(null)}
+        >
+          <div>park detail</div>
+        </InfoWindow>
+      )}
+    </GoogleMap>
+  )
+}
+
+const WrappedMap = withScriptjs(withGoogleMap(Map));
 
 function City(props) {
 
   return (
-    <div>
-      Merhaba
+    <div style={{width:"100vw", height:"100vh"}} >
+      <WrappedMap  
+     googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
+     loadingElement={<div style={{ height: `100%` }} />}
+     containerElement={<div style={{ height: `100%` }} />}
+     mapElement={<div style={{ height: `100%` }} />} />
     </div>
   );
 }
