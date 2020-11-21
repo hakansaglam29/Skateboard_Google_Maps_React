4e880668245b8e5d7d80a0cfb04b17588505889f
Hakan <67585490+hakansaglam29@users.noreply.github.com>
2020-11-21 19:43:02 +0100

    first commit

---
+++ src/restaurant.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect} from "react";
+import axios from "axios";
+
+function ()
+
+
+function City(props) {
+
+  return (
+    <div>
+      Merhaba
+    </div>
+  );
+}
+export default City;
+
+// http://opentable.herokuapp.com/api/cities  
